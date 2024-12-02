8f743eecb1018d3ba952ffc1a263bf9760128804
Don Turner <donturner@google.com>
2024-12-02 18:25:16 +0000

    First 2 days

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        /**
+         * Pair up the smallest number in the left list with the smallest number in the right list,
+         * then the second-smallest left number with the second-smallest right number, and so on.
+         *
+         * Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances.
+         */
+        val (list1, list2) = input.map {
+            val (first, second) = it.split("   ")
+            first.toInt() to second.toInt()
+        }.unzip()
+
+        return list1.sorted().zip(list2.sorted()).sumOf {
+            abs(it.first - it.second)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""".split("\n").filter { it.isNotEmpty() }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // Check that our test input gives the correct result.
+    check(part1(testInput) == 11)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
+    // Run on the real input.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }
---
+++ src/Day02.kt
@@ -0,0 +1,73 @@
+import kotlin.math.abs
+
+fun main() {
+
+    val testInput = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+""".split("\n").filter { it.isNotEmpty() }
+
+    check(part1(testInput) == 2)
+
+    val input = readInput("Day02-1")
+    part1(input).println()
+
+    check(part2(testInput) == 4)
+    part2(input).println()
+}
+
+fun part1(input: List<String>): Int {
+    val reports = input.map {
+        it.split(" ").map { level ->
+            level.toInt()
+        }
+    }
+    return reports.count { it.isSafe() }
+}
+
+fun part2(input: List<String>): Int {
+    val reports = input.map {
+        it.split(" ").map { level ->
+            level.toInt()
+        }
+    }
+
+    return reports.count { report ->
+        report.dampenedReports().any { it.isSafe() }
+    }
+}
+
+fun List<Int>.isSafe(): Boolean {
+    // Rule 1: Values are increasing or decreasing.
+    return if (isIncreasing() || isDecreasing()){
+        // Rule 2: Any two adjacent levels differ by at least one and at most three.
+        isWithinSafeChanges()
+    } else {
+        false
+    }
+}
+
+fun List<Int>.isIncreasing() =
+    zipWithNext().all { it.first < it.second }
+
+fun List<Int>.isDecreasing() =
+    zipWithNext().all { it.first > it.second }
+
+fun List<Int>.isWithinSafeChanges() =
+    zipWithNext().all { abs(it.first - it.second) in 1..3 }
+
+fun List<Int>.dampenedReports() : List<List<Int>> {
+
+    val dampenedReports = mutableListOf(this)
+
+    // Sequentially remove one level to create new reports.
+    forEachIndexed { parentIndex, _ ->
+        dampenedReports.add(this.filterIndexed { childIndex, _ -> childIndex != parentIndex })
+    }
+
+    return dampenedReports
+}
